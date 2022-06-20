42e8dbf7227431a11ef25700154b0f6f23d10863
kangwang <kangwang@doodlemobile.com>
2022-06-20 19:58:09 +0800

    init res

---
+++ core/src/com/tony/rider/android/egl/App.java
@@ -0,0 +1,7 @@
+package com.tony.rider.android.egl;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ core/src/com/tony/rider/android/resolutionstrategy/App.java
@@ -0,0 +1,6 @@
+package com.tony.rider.android.resolutionstrategy;
+
+//填充策略   一个是占满整个屏幕    使用固定的尺寸
+public class App {
+
+}
